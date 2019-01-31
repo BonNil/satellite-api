c6c59bec0667e13604386732214f9e8fd76976c9
Nilsson Bonny <bonny.nilsson@stenaline.com>
2019-01-31 09:56:00 +0100

    Added SkipLayoutServiceRequest processor as per A.Webers workaround suggestion for issue 145 on jss github

---
+++ satellite-api/Processors/SkipLayoutServiceRequest.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.LayoutService.Mvc.Routing;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Owin.Authentication.Pipelines.CookieAuthentication.ApplyRedirect;
+
+namespace satellite_api.Processors
+{
+    public class SkipLayoutServiceRequest : ApplyRedirectProcessor
+    {
+        protected readonly IRouteMapper RouteMapper;
+
+        public SkipLayoutServiceRequest(IRouteMapper routeMapper)
+        {
+            Assert.ArgumentNotNull(routeMapper, "routeMapper");
+            RouteMapper = routeMapper;
+        }
+
+        public override void Process(ApplyRedirectArgs args)
+        {
+            Assert.ArgumentNotNull(args, nameof(args));
+
+            if (PageContext.Current != null && PageContext.Current.RequestContext != null && RouteMapper.IsLayoutServiceRoute(PageContext.Current.RequestContext))
+            {
+                args.Context.RedirectUri = null;
+                args.AbortPipeline();
+            }
+        }
+    }
+}
